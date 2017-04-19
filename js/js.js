5ac71d4ebbd85873fd82459e9f98b0350680895d
Jaber Ibne Mahboob <jaber.ibne.mahboob@gmail.com>
2017-04-19 10:22:52 +0200

    draft till frame 7

--- js/js.js
+++ js/js.js
@@ -1,6 +1,9 @@
 //GENERAL VARIABLE
 let frames = document.querySelectorAll(".frame");
 let backgroundsound = document.querySelector(".background_sound");
+let backgroundsound2 = document.querySelector(".background_sound2");
+let jarofliessound = document.querySelector(".jar_of_lies_sound");
+let boomsound = document.querySelector(".boom_sound");
 
 
 //FRAME #1 VARIABLE
@@ -22,6 +25,31 @@ let frame4 = document.querySelector(".frame4");
 
 
 //FRAME #5 VARIABLE
+let frame5 = document.querySelector(".frame5");
+let frame5_dragable_elements = document.querySelectorAll(".frame5 .dragdrop");
+let frame5_easter_eggs = document.querySelectorAll(".frame5 .eastereggs");
+let frame5_drop_count = 0;
+let frame5_drop_target_index = -1;
+let frame5_bar = document.querySelector(".frame5 .element_bar");
+let frame5_bar_display = document.querySelector(".frame5 .element_bar_display");
+let frame5_target_dropbox = document.querySelector(".frame5 .dropbox");
+
+
+//FRAME #6 VARIABLE
+let frame6 =document.querySelector(".frame6");
+let frame6_bomb = document.querySelector(".frame6 .bomb");
+let frame6_target_dropbox = document.querySelector(".frame6 .dropbox");
+
+
+
+//FRAME #7 VARIABLE
+let frame7 =document.querySelector(".frame7");
+let frame7_boom = document.querySelector(".frame7 .boombox");
+
+
+
+//FRAME #8 VARIABLE
+let frame8 =document.querySelector(".frame8");
 
 
 //GENRAL FUNCTIONS/CONTROL
@@ -33,10 +61,9 @@ function removeCurrentFrameClass(frameItem, index){
 }
 backgroundsound.addEventListener("ended",startBackgroundSoundAgain);
 function startBackgroundSoundAgain(){
-    backgroundsound.currentTime = 1;
+    backgroundsound.currentTime = 40;
     backgroundsound.play();
 }
-backgroundsound.play();
 
 
 //FRAME #1 ANIMATION CONTROL
@@ -49,6 +76,7 @@ function goframe2(){
     console.log(frame2);
     hideCurrentFrame();
     frame2.classList.add("currentFrame");
+    backgroundsound.play();
 }
 frame2_char_piggy.addEventListener("animationend",goframe3);
 
@@ -67,7 +95,139 @@ function goframe4(){
     console.log(frame4);
     hideCurrentFrame();
     frame4.classList.add("currentFrame");
+    jarofliessound.play();
 }
+jarofliessound.addEventListener("ended",goframe5);
 
 
 //FRAME #5 ANIMATION CONTROL
+function goframe5(){
+    console.log(frame5);
+    hideCurrentFrame();
+    frame5.classList.add("currentFrame");
+    console.log(frame5_dragable_elements.length);
+    frame5_dragable_elements.forEach(handle_dragdrop);
+    frame5_easter_eggs.forEach(handle_eastereggs);
+}
+frame5_target_dropbox.addEventListener("dragenter",function(event){
+    event.preventDefault();
+    console.log("dragenter");
+},false);
+frame5_target_dropbox.addEventListener("drop",function(event){
+    console.log("drop => " + "target index: "+frame5_drop_target_index);
+    if(frame5_drop_target_index >= 0){
+        let thisitem = frame5_dragable_elements[frame5_drop_target_index];
+        thisitem.classList.remove("draggingstop");
+        thisitem.classList.add("droppintarget");
+        //setTimeout(function(){
+        //    thisitem.classList.add("frame5_hide");
+        //},1000);
+        frame5_drop_count = frame5_drop_count + 1;
+    }
+    if(frame5_drop_count >= 3){
+        frame5_bar.classList.add("disappear");
+        frame5_bar_display.classList.add("disappear");
+        frame5_bar_display.addEventListener("animationend",goframe6);
+    }
+},false);
+frame5_target_dropbox.addEventListener("dragover",function(event){
+    event.preventDefault();
+    console.log("dragover");
+},false);
+function handle_dragdrop(item, index){
+    item.addEventListener("dragstart",function(){
+        console.log("dragstart");
+        item.style.opacity = '0.6';
+        item.classList.remove("draggingstop");
+        item.classList.add("draggingstart");
+        frame5_drop_target_index = index;
+    },false);
+    item.addEventListener("dragend",function(){
+        console.log("dragend");
+        item.style.opacity = '1';
+        item.classList.remove("draggingstart");
+        item.classList.add("draggingstop");
+        frame5_drop_target_index = -1;
+    },false);
+}
+function handle_eastereggs(item, index){
+    item.addEventListener("click",function(){
+        item.classList.add("eastereggsdisappears");
+        item.addEventListener("animationend",function(){
+            item.classList.remove("frame5_show");
+            item.classList.add("frame5_hide");
+        })
+        if(index < 2){ //largest index is 2
+            let newItem = frame5_easter_eggs[(index+1)];
+            newItem.classList.remove("frame5_hide");
+            newItem.classList.add("frame5_show");
+            newItem.classList.add("eastereggsappears");
+        }
+    });
+}
+
+
+//FRAME #6 ANIMATION CONTROL
+function goframe6(){
+    console.log(frame6);
+    hideCurrentFrame();
+    frame6.classList.add("currentFrame");
+    frame6_bomb.addEventListener("dragstart",function(){
+        console.log("dragstart");
+        frame6_bomb.style.opacity = '0.6';
+        frame6_bomb.classList.remove("draggingstop");
+        frame6_bomb.classList.add("draggingstart");
+    });
+    frame6_bomb.addEventListener("dragend",function(){
+        console.log("dragend");
+        frame6_bomb.style.opacity = '1';
+        frame6_bomb.classList.remove("draggingstart");
+        frame6_bomb.classList.add("draggingstop")
+    })
+}
+frame6_target_dropbox.addEventListener("dragenter",function(event){
+    event.preventDefault();
+    console.log("dragenter");
+},false);
+frame6_target_dropbox.addEventListener("drop",function(event){
+    frame6_bomb.classList.remove("draggingstop");
+    frame6_bomb.classList.add("droppintarget");
+    frame6_bomb.addEventListener("animationend",goframe7);
+},false);
+frame6_target_dropbox.addEventListener("dragover",function(event){
+    event.preventDefault();
+    console.log("dragover");
+},false);
+
+
+//FRAME #7 ANIMATION CONTROL
+function goframe7(){
+    console.log(frame7);
+    hideCurrentFrame();
+    frame7.classList.add("currentFrame");
+    //change sound
+    backgroundsound.pause();
+    backgroundsound.currentTime = 40;
+    backgroundsound2.play();
+
+    setTimeout(function(){boomsound.play()}, 10);
+
+    frame7_boom.addEventListener("animationend",setTimeout(function(){
+        console.log("?");
+        goframe8();
+    }, 2000));
+}
+
+
+//FRAME #8 ANIMATION CONTROL
+function goframe8(){
+    console.log(frame8);
+    hideCurrentFrame();
+    frame8.classList.add("currentFrame");
+}
+
+
+
+
+
+
